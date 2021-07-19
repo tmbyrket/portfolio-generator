73f12e2ea4325354b0857b0f37a86149bcb5ef5f
Reese B <theresa.byrket@gmail.com>
2021-07-18 20:43:44 -0500

    updated template

--- app.js
+++ app.js
@@ -129,11 +129,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        const pageHTML = generatePage(portfolioData);
+       const pageHTML = generatePage(portfolioData);
 
-        //fs.writeFile('./index.html', pageHTML, err => {
-            //if (err) throw new Error(err);
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
 
-            //console.log('Page created! Check out index.html in this directory to see it!');
-        //});
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
